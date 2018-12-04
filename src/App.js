e2f4cf42dc4dea1d276845393f46f5bbf756021d
Magnus <magnus.thor.ar@gmail.com>
2018-12-04 11:39:29 +0100

    create Person component passing information / functions to person component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,58 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: 'Max', age: 28 },
+      { name: 'Manu', age: 30 },
+      { name: 'Stephanie', age: 22 }
+    ]
+  }
+
+  switchNameHandler = (newName) => {
+    // console.log('was clicked');
+    this.setState({
+      persons: [
+        { name: newName, age: 28 },
+        { name: 'Manu', age: 30 },
+        { name: 'Stephanie', age: 34 }
+      ]
+    })
+  }
+
+  nameChangedHandler = (event) => {
+    this.setState({
+      persons: [
+        { name: 'Max', age: 28 },
+        { name: event.target.value, age: 30 },
+        { name: 'Stephanie', age: 22 }
+      ]
+    })
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
+        <p>This is really working!</p>
+        <button onClick={() => this.switchNameHandler('Magnus')}>Switch Name</button>
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age} />
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+          click={this.switchNameHandler.bind(this, 'Oliver')}
+          changed={this.nameChangedHandler} >My Hobbies: Racing</Person>
+        <Person
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age} />
       </div>
     );
+    // Prefer the bind method over arrow function 
+    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
   }
 }
 
